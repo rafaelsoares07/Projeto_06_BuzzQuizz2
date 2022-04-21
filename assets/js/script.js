c56886092b9272d82c4be0d1df6396ee1983c15d
Flavia Bulad <flavia.bulad@gmail.com>
2022-04-21 16:26:14 -0300

    adicionada animação no menu das perguntas

--- assets/js/script.js
+++ assets/js/script.js
@@ -159,143 +159,113 @@ function formatarObjetoQuizz(){
     return objeto
 }
 
-
-
 //Faz o post do objeto criado (quizz do usuário)
 //Coloquei o objeto que tava lá no notion e está dando certo com ele, agora é so pegar as 
 //entradas do usuario e criar o nosso objeto
-function createQuizz(){
-    let promisse = axios.post(`${API}`,formatarObjetoQuizz()) 
-    
-    promisse.then(response =>{
-        
-        console.log(response.data)
-    })
-    
-    promisse.catch(error => alert('Você não preencheu todos os campos para criar o objeto corretamente'))
-}
-
-
-
-//getQuizzes()
-//getQuizzForID()
-//createQuizz()
-
-//renderiza a pág 9 no figma - Tela de criação das perguntas do quizz  
 function renderQuestionsPage() {
     const questionsPage = document.querySelector(".create-questions-page");
     questionsPage.innerHTML += `
-        <div class="questions-container">
-        <p class="titulo-perguntas">Crie suas perguntas</p>
-        
-        <div class="form-container">
-        <div class="subtitulo-botao-container">
-        <span class="subtitulo">Pergunta 1</span>
-        <span class="toggle">
-        <ion-icon name="create-outline"></ion-icon>
-        </span>
-        </div>
-        
-        <input
-        id="pergunta"
-        type="text"
-        placeholder="Texto da página"
-        required
-        minlength="20"
-        />
-        <input
-        id="cor-de-fundo"
-        type="text"
-        placeholder="Cor de fundo da pergunta"
-        pattern="^#+([a-fA-F0-9]{6}|[a-fA-F0-9]{3})$"
-        required
-        />
-        
-        <span class="subtitulo">Resposta Correta</span>
-        <input
-        id="resposta-correta"
-        type="text"
-        placeholder="Resposta correta"
-        required
-        />
-        <input
-        id="url-imagem-resposta"
-        type="url"
-        placeholder="URL da imagem"
-        pattern="https://.*"
-        size="30"
-        required
-        />
-        
-        <span class="subtitulo">Respostas Incorretas</span>
-        <input
-        id="resposta-incorreta-1"
-        type="text"
-        placeholder="Resposta incorreta 1"
-        required
-        />
-        <input
-        id="url-imagem"
-        type="url"
-        placeholder="URL da imagem 1"
-        pattern="https://.*"
-        size="30"
-        required
-        />
-        
-        <input
-        id="resposta-incorreta-2"
-        type="text"
-        placeholder="Resposta incorreta 2"
-        />
-        <input id="url-imagem" type="url" placeholder="URL da imagem 3"
-        pattern="https://.*" size="30"/>
-        
-        <input
-        id="resposta-incorreta-3"
-        type="text"
-        placeholder="Resposta incorreta 3"
-        />
-        <input
-        id="url-imagem"
-        type="url"
-        placeholder="URL da imagem 3"
-        pattern="https://.*"
-        size="30"
-        />
-        </div>
-        
-        <div class="collapsable-form ">
-        <div class="subtitulo-botao-container">
-        <span class="subtitulo">Pergunta 2</span>
-        
-        <span class="toggle">
-        <ion-icon name="create-outline"></ion-icon>
-        </span>
-        </div>
-        </div>
-        
-        <div class="collapsable-form ">
-        <div class="subtitulo-botao-container">
-        <span class="subtitulo">Pergunta 3</span>
-        
-        <span class="toggle">
-        <ion-icon name="create-outline"></ion-icon>
-        </span>
-        </div>
-        </div>
-        
-        <button onclick="isUrlValid(this)">Prosseguir para criar níveis</button>
-        
-        </div>
-    `;
-  }
-
+    <p class="titulo-perguntas">Crie suas perguntas</p>
+    <div class="perguntas-container">
+  <button class="collapsible">Pergunta 1<ion-icon name="create-outline"></ion-icon></button>
+  <div class="content">
   
-  /*Função que verifica se a URL inserida é válida -> testar se o input não funcionar */
+  <form>
+          <input id="pergunta" type="text" placeholder="Texto da página" required minlength="20"/>
+          <input id="cor-de-fundo" type="text" placeholder="Cor de fundo da pergunta" pattern="^#+([a-fA-F0-9]{6}|[a-fA-F0-9]{3})$" required/>
+          
+          <span class="subtitulo">Resposta Correta</span>
+          <input id="resposta-correta" type="text" placeholder="Resposta correta" required/>
+          <input id="url-imagem-resposta" type="url" placeholder="URL da imagem" pattern="https?://.+" required />
+          
+          <span class="subtitulo">Respostas Incorretas</span>
+          <input
+          id="resposta-incorreta-1" type="text" placeholder="Resposta incorreta 1" required />
+          <input id="url-imagem-1" type="url" placeholder="URL da imagem 1" pattern="https?://.+" required />
+          
+          <input id="resposta-incorreta-2" type="text" placeholder="Resposta incorreta 2"/>
+          <input id="url-imagem-2" type="url" placeholder="URL da imagem 3" pattern="https?://.+" required />
+          
+          <input id="resposta-incorreta-3" type="text" placeholder="Resposta incorreta 3"/>
+          <input id="url-imagem-3" type="url" placeholder="URL da imagem 3" pattern="https?://.+" required />
+  </form>
+  </div>
+  
+  <div class="perguntas-container">
+  <button class="collapsible">Pergunta 2<ion-icon name="create-outline"></ion-icon></button>
+  <div class="content">
+  
+  <form>
+          <input id="pergunta" type="text" placeholder="Texto da página" required minlength="20"/>
+          <input id="cor-de-fundo" type="text" placeholder="Cor de fundo da pergunta" pattern="^#+([a-fA-F0-9]{6}|[a-fA-F0-9]{3})$" required/>
+          
+          <span class="subtitulo">Resposta Correta</span>
+          <input id="resposta-correta" type="text" placeholder="Resposta correta" required/>
+          <input id="url-imagem-resposta" type="url" placeholder="URL da imagem" pattern="https?://.+" required />
+          
+          <span class="subtitulo">Respostas Incorretas</span>
+          <input
+          id="resposta-incorreta-1" type="text" placeholder="Resposta incorreta 1" required />
+          <input id="url-imagem-1" type="url" placeholder="URL da imagem 1" pattern="https?://.+" required />
+          
+          <input id="resposta-incorreta-2" type="text" placeholder="Resposta incorreta 2"/>
+          <input id="url-imagem-2" type="url" placeholder="URL da imagem 3" pattern="https?://.+" required />
+          
+          <input id="resposta-incorreta-3" type="text" placeholder="Resposta incorreta 3"/>
+          <input id="url-imagem-3" type="url" placeholder="URL da imagem 3" pattern="https?://.+" required />
+  </form>
+  </div>
+  
+  <div class="perguntas-container">
+  <button class="collapsible">Pergunta 3<ion-icon name="create-outline"></ion-icon></button>
+  <div class="content">
+  
+  <form>
+          <input id="pergunta" type="text" placeholder="Texto da página" required minlength="20"/>
+          <input id="cor-de-fundo" type="text" placeholder="Cor de fundo da pergunta" pattern="^#+([a-fA-F0-9]{6}|[a-fA-F0-9]{3})$" required/>
+          
+          <span class="subtitulo">Resposta Correta</span>
+          <input id="resposta-correta" type="text" placeholder="Resposta correta" required/>
+          <input id="url-imagem-resposta" type="url" placeholder="URL da imagem" pattern="https?://.+" required />
+          
+          <span class="subtitulo">Respostas Incorretas</span>
+          <input
+          id="resposta-incorreta-1" type="text" placeholder="Resposta incorreta 1" required />
+          <input id="url-imagem-1" type="url" placeholder="URL da imagem 1" pattern="https?://.+" required />
+          
+          <input id="resposta-incorreta-2" type="text" placeholder="Resposta incorreta 2"/>
+          <input id="url-imagem-2" type="url" placeholder="URL da imagem 3" pattern="https?://.+" required />
+          
+          <input id="resposta-incorreta-3" type="text" placeholder="Resposta incorreta 3"/>
+          <input id="url-imagem-3" type="url" placeholder="URL da imagem 3" pattern="https?://.+" required />
+  </form>
+  </div>
+
+  <form><input class="botao-prosseguir-perguntas" type="submit" value="Prosseguir para criar níveis"></form>
+  </div>
+    `;
+    openCollapsable();
+  }
+  //Função para abrir o formulário de perguntas
+  function openCollapsable() {
+    let coll = document.getElementsByClassName("collapsible");
+    for (let i = 0; i < coll.length; i++) {
+      coll[i].addEventListener("click", function () {
+        this.classList.toggle("active");
+        let content = this.nextElementSibling;
+        if (content.style.maxHeight) {
+          content.style.maxHeight = null;
+        } else {
+          content.style.maxHeight = content.scrollHeight + "px";
+        }
+      });
+    }
+  }
+  
+  //Função que verifica se a URL inserida é válida -> testar se o input não funcionar 
 
   function isUrlValid(element) {
-    let input = document.getElementById("url-imagem");
+    let input = document.getElementById(".url-imagem");
     let verifica = input.match(
       /(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/g
     );
